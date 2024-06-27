a8bb3eca6a4cf04416f91b5ecfe2a55d3317452e
Alrn <146360711+Annabelle-Lrn@users.noreply.github.com>
2024-06-27 17:30:55 +0200

    #

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-// const carrouImg = document.querySelectorAll(".carrousel-item");
-// console.log(carrouImg);
-// let index =0;
-// function showImg(index){
-//     if(carrouImg[index].classList.contains("active")){
-//         carrouImg[index].classList.remove("active");
-//     }
-//     else{
-//     carrouImg[index].classList.add('active');
-//     index++;
-//     carrouImg[index-1].classList.remove('active');
-// }
-// };
-// setInterval(() => {showImg()}, 3000);
-
 const carrouImg = document.querySelectorAll(".carrousel-item");
 const descrip = document.getElementsByTagName("p");
+
 let index = 0;
-
-
-
 
 function showImg(){  
     carrouImg[index].classList.remove('active');
     descrip[index].classList.remove('active');
     index++;
+
     if (index>=carrouImg.length-1){
         index=0;
         carrouImg[carrouImg.length-1].classList.remove('active');
@@ -31,13 +15,20 @@ function showImg(){
         carrouImg[index].classList.add("active");
         descrip[index].classList.add("active")
     }
+
     carrouImg[index].classList.add('active');
     descrip[index].classList.add('active');
-
-
+        
     carrouImg[index].addEventListener("click",(e)=>{
-    console.log(e)
-    clearInterval(t);
-})
+        console.log(e);
+        clearInterval(t);
+    });
 };
+
 let t = setInterval(() => {showImg()}, 3000);
+
+
+// close.addEventListener("click",(e)=>{
+//     lightboxBg.remove("show");
+//     setInterval(() => {showImg()}, 3000);
+// })
